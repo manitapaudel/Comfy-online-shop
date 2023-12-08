75828ae555866d40cf9c7a739c756ad309095116
Manita Paudel <paudel04manita@gmail.com>
2023-12-08 11:48:00 +0545

    Initial pages setup

---
+++ index.js
@@ -0,0 +1,66 @@
+// const featuredProducts = allProducts.filter((product) => product.fields.featured === true);
+// console.log({featuredProducts});
+const shoppingCartIcon = document.getElementsByClassName("fa-shopping-cart")[0];
+shoppingCartIcon.style.color = "white";
+
+const data = [
+    {
+        imageSrc: "assets/hero-bg.jpeg",
+        name: "High-Back Bench",
+        price: "$9.99"
+    },
+    {
+        imageSrc: "assets/hero-bg.jpeg",
+        name: "Utopia Sofa",
+        price: "$48.99"
+    }, 
+    {
+        imageSrc: "assets/hero-bg.jpeg",
+        name: "Entertainment Center",
+        price: "$18.99"
+    }
+]
+
+const featuredSection = document.getElementById("featured-products");
+
+data.map((datum) => {
+    const product = document.createElement("div");
+    const image = document.createElement("img");
+    const footer = document.createElement("div");
+    footer.className = "footer";
+    const productName = document.createElement("p");
+    productName.textContent = datum.name;
+    const productPrice = document.createElement("p");
+    productPrice.textContent = datum.price;
+    image.src = datum.imageSrc;
+    product.className = "product";
+    product.append(image);
+    footer.append(productName);
+    footer.append(productPrice);
+    product.append(footer);
+    featuredSection.append(product);
+    
+    // styling the texts
+    productName.style.color= "#617d98";
+    productName.style.fontSize= "1.3rem";
+    productPrice.style.fontSize= "1rem";
+    productPrice.style.fontWeight= "600";
+    
+    // styling the images
+    image.style.height = "13rem";
+    image.style.width = "22rem";
+    image.style.borderRadius = "4px"
+})
+
+// product.textContent = "Hi! Can you see me though?"
+
+async function getProducts() {
+    const response = await fetch("https://course-api.com/javascript-store-products");
+    const products = await response.json();
+    allProducts = products;
+    console.log(products);
+    return products;
+}
+// getProducts();
+// let allProducts = [];
+// console.log("Here", allProducts);
