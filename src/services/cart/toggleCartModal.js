03f94729c0a7878d6487bdfdf5cf38a0a04bf626
Manita Paudel <paudel04manita@gmail.com>
2023-12-24 20:09:40 +0545

    Update the cart modal logic, add google fonts link to the pages other than index

--- src/services/cart/toggleCartModal.js
+++ src/services/cart/toggleCartModal.js
@@ -1,18 +1,15 @@
 import { displayTotal } from "./cartTotal.js";
 import displayCartItems from "./displayCartItems.js";
 
-const cartButtons = document.querySelectorAll(".cart");
+const cartButton = document.querySelector(".cart");
 const cartModalOverlay = document.querySelector(".cart-modal-overlay");
 const closeCart = document.querySelector("#cart-close");
-console.log("hi", cartButtons)
 
-cartButtons.forEach((cartButton) =>{
   cartButton.addEventListener("click", () => {
     cartModalOverlay.classList.add("show");
-    console.log("is it working??");
+    displayTotal();
+    displayCartItems();
   })
-  displayTotal();
-});
 
 closeCart.addEventListener("click", () => {
   cartModalOverlay.classList.remove("show");
@@ -20,6 +17,5 @@ closeCart.addEventListener("click", () => {
 
 export const openCartModal = () => {
   cartModalOverlay.classList.add("show");
-  displayCartItems();
   displayTotal();
 }
