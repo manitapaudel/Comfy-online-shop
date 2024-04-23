fe7d996c222459fede32f2af97fb68f2a7f04967
Manita Paudel <paudel04manita@gmail.com>
2024-04-23 14:48:22 +0545

    Add button styles

--- src/services/products/displaySingleProduct.js
+++ src/services/products/displaySingleProduct.js
@@ -8,7 +8,7 @@ const displaySingleProduct = (product, element) => {
                     <p class="company">By ${product.company.toUpperCase()}</p>
                     <p class="price">$${product.price / 100}</p>
                     <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam iusto eum voluptatem tenetur sunt dolor at ullam suscipit vel amet.</p>
-                    <button>Add to Cart</button>
+                    <button class="primary-btn all-products-btn" >Add to Cart</button>
                 </div>
             `;
 };
